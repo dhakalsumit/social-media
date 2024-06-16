47091e4a58fe149835b2ec77a78aba7b66e014e0
Sumit Dhakal <dhakalsumeet123@gmail.com>
2024-06-16 18:09:02 +0545

    google sign in

--- lib/core/common/sign_in_button.dart
+++ lib/core/common/sign_in_button.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:social_media/core/constant/constant.dart';
+import 'package:social_media/features/auth/controller/auth_controller.dart';
 
-class Signinbutton extends StatelessWidget {
+class Signinbutton extends ConsumerWidget {
   const Signinbutton({super.key});
 
+  void signInWithGoogle(WidgetRef ref) {
+    ref.read(authControllerProvider).signInWithgoogle();
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.all(18.0),
       child: ElevatedButton.icon(
-          onPressed: () {},
+          onPressed: () {
+            signInWithGoogle(ref);
+          },
           icon: Image.asset(
             Constants.googlePath,
             width: 30,
